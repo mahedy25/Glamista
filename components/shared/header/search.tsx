7db1bc77c2922b2f1621a89aaab3260a87b31691
mahedy25 <mahedyhasan873@gmail.com>
2025-04-18 21:48:45 +0600

    added header

---
+++ components/shared/header/search.tsx
@@ -0,0 +1,50 @@
+import { SearchIcon } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { APP_NAME } from '@/lib/constants'
+const categories = ['men', 'women', 'kids', 'accessories']
+export default async function Search() {
+  return (
+    <form action='/search' method='GET' className='flex items-stretch h-10'>
+  {/* Category Dropdown */}
+  <Select name='category'>
+    <SelectTrigger className='min-h-[40px] h-[40px]  rounded-none rounded-l-md border border-gray-300 bg-gray-100 text-black font-semibold px-3'>
+      <SelectValue className='text-black font-semibold' placeholder='All' />
+    </SelectTrigger>
+    <SelectContent position='popper'>
+      <SelectItem value='all'>All</SelectItem>
+      {categories.map((category) => (
+        <SelectItem key={category} value={category}>
+          {category}
+        </SelectItem>
+      ))}
+    </SelectContent>
+  </Select>
+
+  {/* Search Input */}
+  <Input
+    name='q'
+    type='search'
+    placeholder={`Search Site ${APP_NAME}`}
+    className='h-full rounded-none border-y border-gray-300 bg-gray-100 font-semibold text-black text-base px-3'
+  />
+
+  {/* Submit Button */}
+  <button
+    type='submit'
+    className='h-full px-3 rounded-none rounded-r-md bg-primary text-black border border-gray-300 border-l-0 flex items-center justify-center'
+  >
+    <SearchIcon className='w-5 h-5' />
+  </button>
+</form>
+
+
+  )
+}
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Glamista'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Unleash Your Style — Wear the Trend, Be the Trend.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "An Ecommerce Website Created With Next.js, MongoDB, Tailwind CSS, TypeScript and Next Auth."
